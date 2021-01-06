4842e32683a52b102294c62aa98b5ad4d46be64d
Eiji KOMINAMI <eiji@surbiton.jp>
2021-01-06 13:34:31 +0900

    Update the 'python' module.

--- roles/certbot/files/update_cert.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-/bin/certbot renew --force-renewal --no-self-upgrade --server https://acme-v02.api.letsencrypt.org/directory --agree-tos --dns-route53
-/usr/local/apache2/bin/apachectl restart
